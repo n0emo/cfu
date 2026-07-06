4702580ede3bfb8849fa2399bda105fc41ae7a48
Albert Shefner <usn0emo@gmail.com>
2026-07-07 00:49:30 +0300

    Remove Edyn, return Box2D

--- src/game/constants.hpp
+++ src/game/constants.hpp
@@ -5,4 +5,6 @@ namespace game {
 constexpr auto WINDOW_WIDTH = 720;
 constexpr auto WINDOW_HEIGHT = 720;
 
+constexpr auto B2_STEPS = 4;
+
 } // namespace game
--- src/game/physics/components.hpp
+++ src/game/physics/components.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <box2d/box2d.h>
 #include <entt/entt.hpp>
 #include <imgui_entt_entity_editor.hpp>
 #include <raylib.h>
 
 namespace game::components {
+
+struct World {
+    b2WorldId id;
+};
 
 struct Position {
     Vector2 vec;
